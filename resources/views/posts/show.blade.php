139a980bcd422446ae86bdb76366dfa544ac9bc0
frank <59645305+frapgn@users.noreply.github.com>
2020-05-20 23:23:46 +0200

    mi ero dimenticato l'immagine

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,7 @@
 <body>
     <article>
         <h2>{{$post->title}}</h2>
+        <img src="{{$post->url_image}}" alt="{{$post->title}}">
         <small>{!!'<strong>' . $post->author . '</strong>'!!}</small>
         <small>{{$post->created_at}}</small>
         <p>{{$post->paragraph}}</p>
